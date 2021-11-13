39a7376f15d9c529e2a3b1749072cd7f47ead79b
aparnac624 <aparnac624@gmail.com>
2021-11-13 20:39:01 +0530

    changes4

--- form.js
+++ form.js
@@ -1,143 +1,126 @@
 var email = document.getElementById("email");
-var error1 = document.getElementById("error1");
-var error2 = document.getElementById("error2");
-var pwd = document.getElementById("pwd");
+var password = document.getElementById("pwd");
 
-// const form=document.getElementsByClassName("loginclass");
 
-// form.addEventListener('submit',function(event){
-//     // event.preventDefault();
-//     if(validate()==false || passCheck()==false || passWordstrength()==false){
-//         return false;
-//     }
-//     else{
-//         return true;
-//     }
-// }
+// email validation start
 
-function validate(){
-    // if(email.value=="" || pwd.value==""){
-    //     alert("The fields cannot be empty");
-    //     return false;
-    // }
-    // else{
-    //     // alert("the validation is proper");
-    //     return true;
-      
-    // }
-
+function emailvalidate(emailinput){
     let regexp = /^([A-Za-z0-9\.\-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-    if(regexp.test(email.value)){
-        error1.innerHTML="Email is valid";
-        error1.style.visibility="visible";
-        error1.style.color="green";
-        return true;
+    if(regexp.test(emailinput)==true){
+        error1.innerHTML = "Email is valid";
+        error1.style.visibility = "visible";
+        error1.style.color = "green";
+        // return true;
+        flag=1;
     }
     else{
-        error1.innerHTML="Invalid email";
-        error1.style.visibility="visible";
-        error1.style.color="red";
-        return false;
+        error1.innerHTML = "Email is invalid";
+        error1.style.visibility = "visible";
+        error1.style.color = "red";
+        // return false;
+        flag =0;
     }
 }
 
-function passCheck(data){
-    var password = document.getElementsByClassName("passwordcheck");
+// email validation end
+
+// password validation start
+
+function passwordvalidate(passwordinput){
+    var rules = document.getElementsByClassName("passwordcheck");
     const eightChar = new RegExp('(?=.{8,})');
     const lowercase = new RegExp('(?=.*[a-z])');
     const uppercase = new RegExp('(?=.*[A-Z])');
     const number = new RegExp('(?=.*[0-9])');
     
 
-    if(eightChar.test(data)){
-       password[0].style.color = "springgreen";
+    if(eightChar.test(passwordinput)==true){
+       rules[0].style.color = "springgreen";
        
       
     }
     else{
-        password[0].style.color = "grey";
-        return false;
+        rules[0].style.color = "grey";
+        flag=0;
         
     }
 
-    if(lowercase.test(data)){
-        password[1].style.color = "springgreen";
+    if(lowercase.test(passwordinput)==true){
+        rules[1].style.color = "springgreen";
         
         
     }
     else{
-        password[1].style.color = "grey";
-        return false;
+        rules[1].style.color = "grey";
+        flag=0;
         
     }
 
-    if(uppercase.test(data)){
-        password[2].style.color = "springgreen";
+    if(uppercase.test(passwordinput)==true){
+        rules[2].style.color = "springgreen";
         
         
     }
     else{
-        password[2].style.color = "grey";
-        return false;
+        rules[2].style.color = "grey";
+        flag=0;
         
     }
 
-    if(number.test(data)){
-        password[3].style.color = "springgreen";
+    if(number.test(passwordinput)==true){
+        rules[3].style.color = "springgreen";
         
         
     }
     else{
-        password[3].style.color = "grey";
-        return false;
+        rules[3].style.color = "grey";
+        flag=0;
         
     }
-
-    // if(eightChar.test(data)==false || lowercase.test(data)==false || uppercase.test(data)==false || number.test(data)==false)
-    // {
-    //     return false;
-    // }
-    // else{
-    //     return true;
-    // }
-
 }
 
-function passWordstrength(data2){
+// password validation end
+
+// validation of password strength start
+function passwordstrength(passwordinput2){
     var msg = document.getElementById("message");
     var str = document.getElementById("strength");
 
-    if(data2.length > 0){
+    if(passwordinput2.length > 0){
         msg.style.display = "block";
-        if(data2.length < 4){
+        if(passwordinput2.length < 4){
             str.innerHTML = "weak";
             msg.style.color = "red";
-            return false;
+            flag=0;
         }
-        else if(data2.length >=4 && data2.length <8){
+        else if(passwordinput2.length >=4 && passwordinput2.length <8){
             str.innerHTML = "medium";
             msg.style.color = "orange";
-            return false;
+            flag=0;
         }
-        else if(data2.length >=8){
+        else if(passwordinput2.length >=8){
             str.innerHTML = "strong";
             msg.style.color = "springgreen";
-            return true;
+            flag=1;
         }
     }
     else{
         msg.style.display = "none";
     }
 }
+// validation of password strength end
 
-function validation(){
-    if( validate()==false || passCheck()==false || passWordstrength()==false)
-    {
-        return false;
-    }
-    else{
+
+
+// validate function start
+function validate(){
+    if(flag==1){
         return true;
     }
+    else{
+        return false;
+    }
 }
+// validate function end
 
     
